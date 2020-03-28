d1610a8425acb16514dde9a21a685cd8bdf88130
Alberto Penas Amor <alberto.penas@nomasystems.com>
2020-03-28 09:15:11 +0100

    created project

---
+++ SonarQubeDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SonarQubeDemo
+//
+//  Created by Alberto Penas Amor on 26/03/2020.
+//  Copyright © 2020 com.github.albertopeam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SonarQubeDemoTests/SonarQubeDemoTests.swift
@@ -0,0 +1,18 @@
+//
+//  SonarQubeDemoTests.swift
+//  SonarQubeDemoTests
+//
+//  Created by Alberto Penas Amor on 26/03/2020.
+//  Copyright © 2020 com.github.albertopeam. All rights reserved.
+//
+
+import XCTest
+@testable import SonarQubeDemo
+
+class SonarQubeDemoTests: XCTestCase {
+
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+
+}
